6c3f15df8936164b91f2e5f7c0315e12209af1f5
sbagos27 <sbagos@csumb.edu>
2026-03-22 22:03:00 -0700

    commit something

--- lab5-SolarSystem/index.mjs
+++ lab5-SolarSystem/index.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
 const planets = (await import('npm-solarsystem')).default;
-let mercury = planets.getMercury();
 
 const app = express();
 app.set("view engine", "ejs");
@@ -8,21 +7,36 @@ app.use(express.static("public"));
 
 // routes
 // root routes
-app.get('/', (req, res) => {
-    res.render('home.ejs')
+app.get('/', async (req, res) => {
+    let url = "https://pixabay.com/api/?key=5589438-47a0bca778bf23fc2e8c5bf3e&per_page=50&orientation=horizontal&q=solar systemLinks to an external site.";
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data);
+
+    let randomNumber = Math.floor(Math.random() * 50);
+
+    let img = data.hits[randomNumber].webformatURL;
+    
+    res.render('home.ejs', {img})
 });
 
 app.get('/planetInfo', (req, res) => {
     let planet = req.query.planet;
     let planetInfo = planets[`get${planet}`]();
-    res.render('planet.ejs', {planetInfo})
+    console.log(planetInfo);
+    res.render('planetInfo.ejs', {planetInfo, planet})
 });
 
-// app.get('/mercury', (req, res) => {
-//     let mercuryInfo = planets.getMercury();
-//     console.log(mercuryInfo)
-//     res.render('mercury.ejs', {mercuryInfo})
-// });
+app.get('/NASAPOD', async (req, res) => {
+
+    let url = "https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=2026-03-11"
+    let response = await fetch(url);
+    let data = await response.json();
+    let img = data.hdurl;
+    res.render('NasaPod.ejs', {img});
+});
+
 
 app.listen(3000, () => {
     console.log('server started');
